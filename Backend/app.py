2f7bfd77a73f05ff2a56bef0cd1b653d7de6afa7
준영 (Haruster) <67867993+Haruster@users.noreply.github.com>
2023-10-31 12:00:53 +0900

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -1,8 +1,18 @@
-from flask import Flask 
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 
-@app.route('/login.html')
+def hello_world():
+  
+    return 'Hello World!'
 
+@app.route('./login.html')
+
+def login():
+
+  pass
+
+if __name__ == '__main__':
+    app.run()
